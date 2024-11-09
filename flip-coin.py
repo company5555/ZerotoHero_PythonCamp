266422cd830cad2810c56693a88b5a1a57f7e457
company5555 <b.ozcansamsun@gmail.com>
2024-11-09 17:56:12 +0300

    Changes

---
+++ flip-coin.py
@@ -0,0 +1,17 @@
+import random
+
+#flip = random.choice(["Heads","Tails"]) #make choices between in the list elements with same prob.
+
+#print(flip)
+
+#generate = random.randint(1,100) # creates random variable between the specified range.
+
+#print(generate)
+
+
+cards = ["jack","quenn","king","ace"]
+
+random.shuffle(cards)
+
+print(cards)
+
---
+++ pystatistics.py
@@ -0,0 +1,4 @@
+import sys
+
+
+print("helloaaa", sys.argv[1])
